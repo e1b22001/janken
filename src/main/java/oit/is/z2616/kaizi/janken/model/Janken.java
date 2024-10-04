b78e2e5535b05db70d3d0fda9990621259b2ee19
e1b22001 <e1b22001@oit.ac.jp>
2024-10-04 14:14:46 +0900

    じゃんけんの手を選んでCPUと対戦

--- src/main/java/oit/is/z2616/kaizi/janken/model/Janken.java
+++ src/main/java/oit/is/z2616/kaizi/janken/model/Janken.java
@@ -2,6 +2,9 @@ package oit.is.z2616.kaizi.janken.model;
 
 public class Janken {
   String name;
+  String userHand;
+  String cpuHand;
+  String result;
 
   public Janken(String name) {
     this.name = name;
@@ -13,5 +16,29 @@ public class Janken {
 
   public void setName(String name) {
     this.name = name;
+  }
+
+  public String getUserHand() {
+    return userHand;
+  }
+
+  public void setUserHand(String userHand) {
+    this.userHand = userHand;
+  }
+
+  public String getCpuHand() {
+    return cpuHand;
+  }
+
+  public void setCpuHand(String cpuHand) {
+    this.cpuHand = cpuHand;
+  }
+
+  public String getResult() {
+    return result;
+  }
+
+  public void setResult(String result) {
+    this.result = result;
   }
 }
