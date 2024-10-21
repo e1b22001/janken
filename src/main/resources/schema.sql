71a0647fbc200d480877952b7176e991e122de95
e1b22001 <e1b22001@oit.ac.jp>
2024-10-21 14:46:59 +0900

    勝者の表示を追加

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,5 +7,6 @@ CREATE TABLE matches (
     user1 INTEGER NOT NULL,
     user2 INTEGER NOT NULL,
     user1Hand VARCHAR NOT NULL,
-    user2Hand VARCHAR NOT NULL
+    user2Hand VARCHAR NOT NULL,
+    winner VARCHAR NOT NULL --追加
 );
